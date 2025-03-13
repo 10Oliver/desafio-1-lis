e7635825b465d42b6076782180fc9e2d6472885c
10Oliver <oliver.erazo1@gmail.com>
2025-03-13 22:01:05 +0000

    🚧 Working on login design

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,32 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <title>Inicio de Sesión</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 
 <body>
-    <div class="container">
-        <h1>Iniciar Sesión</h1>
-        <form action="{{ route('login.process') }}" method="POST">
-            @csrf
-            <div>
-                <label for="email">Correo electrónico:</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div>
-                <label for="password">Contraseña:</label>
-                <input type="password" id="password" name="password" required>
-                <div>
-                    @if ($errors->any())
-                        @foreach ($errors->all() as $error)
-                            {{ $error }}
-                        @endforeach
-                    @endif
+    <div class="login-container">
+        <div class="login-form">
+            <h4 class="login-title">Inicio de sesión</h4>
+            <form action="{{ route('login.process') }}" method="POST">
+                @csrf
+                <div class="input-field">
+                    <input type="email" id="email" name="email" required>
+                    <div class="first-border"></div>
+                    <div class="last-border"></div>
+                    <label for="email">Usuario</label>
                 </div>
+                <div class="input-field password-input-field">
+                    <input type="password" id="password" name="password" required placeholder="Contraseña">
+                    <div class="first-border"></div>
+                    <div class="last-border"></div>
+                    <label for="password">Contraseña</label>
+                    <div>
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                {{ $error }}
+                            @endforeach
+                        @endif
+                    </div>
 
-            </div>
-            <button type="submit">Entrar</button>
-        </form>
+                </div>
+                <button type="submit" class="submit-button">Iniciar sesión</button>
+            </form>
+        </div>
     </div>
 </body>
 
