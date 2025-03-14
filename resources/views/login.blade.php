81f281313ca56d20d7722aab8bf16d489b21411d
10Oliver <oliver.erazo1@gmail.com>
2025-03-14 02:50:47 +0000

    💄 Login design

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,12 +9,13 @@
 
 <body>
     <div class="login-container">
+        <div class="shadow"></div>
         <div class="login-form">
             <h4 class="login-title">Inicio de sesión</h4>
-            <form action="{{ route('login.process') }}" method="POST">
+            <form action="{{ route('login.process') }}" method="POST" autocomplete="off">
                 @csrf
-                <div class="input-field">
-                    <input type="email" id="email" name="email" required>
+                <div class="input-field email-input-field">
+                    <input type="email" id="email" name="email" required placeholder="Usuario">
                     <div class="first-border"></div>
                     <div class="last-border"></div>
                     <label for="email">Usuario</label>
@@ -24,16 +25,19 @@
                     <div class="first-border"></div>
                     <div class="last-border"></div>
                     <label for="password">Contraseña</label>
-                    <div>
-                        @if ($errors->any())
-                            @foreach ($errors->all() as $error)
-                                {{ $error }}
-                            @endforeach
-                        @endif
-                    </div>
-
+                </div>
+                <div class="error-message">
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            {{ $error }}
+                        @endforeach
+                    @endif
                 </div>
                 <button type="submit" class="submit-button">Iniciar sesión</button>
+                <div class="register-text">
+                    <p>¿Aún no tienes cuenta?</p>
+                    <a href="">Registrate</a>
+                </div>
             </form>
         </div>
     </div>
