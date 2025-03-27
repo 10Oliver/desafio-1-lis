c1c36c8831e8dbcc380933ba3a0b25822a07f394
Vladimir7w7 <107089945+Vladimir7w7@users.noreply.github.com>
2025-03-26 20:58:01 -0600

    Seeder de cuenta y Seeder de DB

---
+++ app/Http/Requests/UpdateAccountRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAccountRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:5|max:255',
+            'account_type_uuid' => 'required',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'El nombre es obligatorio.',
+            'name.string' => 'El nombre debe ser un texto válido.',
+            'name.min' => 'El nombre debe tener al menos 5 caracteres.',
+            'name.max' => 'El nombre no puede superar los 255 caracteres.',
+
+            'account_type_uuid.required' => 'El tipo de cuenta es obligatorio.',
+        ];
+    }
+}
